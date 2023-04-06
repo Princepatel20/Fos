2690afe63e4460e037876ff021a18c58c8bda940
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-04-06 07:36:27 +0530

    Create Exit.c

---
+++ Exit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main ()
+
+{
+
+FILE * pFile;
+
+pFile = fopen ("myfile.txt", "r");
+
+if (pFile == NULL)
+
+{
+
+printf ("Error opening file");
+
+exit (1);
+
+}
+
+Else
+
+{
+
+}
+
